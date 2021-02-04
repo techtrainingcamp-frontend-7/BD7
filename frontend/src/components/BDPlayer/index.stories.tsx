e0d889c7d6acd055682dbb0940ac0bd54c06cf05
upupming <yimingli.cs@gmail.com>
2021-02-04 14:09:41 +0800

    feat: 播放器增加非活跃状态 & 支持直播

--- frontend/src/components/BDPlayer/index.stories.tsx
+++ frontend/src/components/BDPlayer/index.stories.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Meta } from '@storybook/react/types-6-0'
 import { BDPlayerProps, BDPlayer } from '.'
+import { Button } from '../Button'
 
 import './index.stories.less'
 
@@ -16,6 +17,7 @@ const bdPlayerProps: BDPlayerProps = {
   videoUrl:
     'https://picgo-1256492673.cos.ap-chengdu.myqcloud.com/RW20seconds_1.mp4',
 }
+// 点播播放器的使用方法
 export const bdPlayer = () => (
   <div className="home">
     <div className="home-main">
@@ -24,3 +26,36 @@ export const bdPlayer = () => (
     <div className="home-tabs"></div>
   </div>
 )
+
+const livePlayerProps: BDPlayerProps = {
+  videoUrl: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
+}
+
+// 直播播放器的使用方法
+export const livePlayer = () => (
+  <div className="home">
+    <BDPlayer {...livePlayerProps} />
+  </div>
+)
+
+// active 状态转换的使用方法
+export const bdPlayerActiveness = () => {
+  const [active, setActive] = useState(false)
+
+  return (
+    <div className="home">
+      <div className="home-main">
+        <BDPlayer {...bdPlayerProps} active={active} />
+      </div>
+      <div className="home-tabs">
+        <Button
+          label={active ? '禁用' : '启用'}
+          onClick={() => {
+            setActive(!active)
+          }}
+          primary
+        />
+      </div>
+    </div>
+  )
+}
--- frontend/src/components/BDPlayer/index.tsx
+++ frontend/src/components/BDPlayer/index.tsx
@@ -1,14 +1,19 @@
 import React, { useRef, useState } from 'react'
 import classnames from 'classnames'
 import PlayIcon from '../static/img/play.svg'
+import LoadingIcon from '../static/img/loading.svg'
+import { useAsync } from 'react-use'
+import Hls from 'hls.js'
 
 import './index.less'
 
 export interface BDPlayerProps {
-  /* 视频地址或者直播地址 */
+  /* 视频地址或者直播地址，目前只支持「.m3u8」格式的 HLS 直播 */
   videoUrl: string
   /* 视频封面 */
   videoPosterUrl?: string
+  /* 是否处于活跃状态，非活跃状态下不会播放，默认为 `true` */
+  active?: boolean
   /* 自定义类名 */
   className?: string
 }
@@ -17,27 +22,72 @@ export const BDPlayer: React.FC<BDPlayerProps> = ({
   videoUrl,
   className,
   videoPosterUrl,
+  active = true,
 }) => {
   const [playing, setPlaying] = useState(false)
+  const [loading, setLoading] = useState(false)
   const [videoDuration, setVideoDuration] = useState(1)
   const [currentTime, setCurrentTime] = useState(0)
   const videoRef = useRef<HTMLVideoElement>(null)
+
+  const startPlay = async () => {
+    if (!videoRef.current) return
+
+    setLoading(true)
+    try {
+      await videoRef.current.play()
+      // Automatic playback started!
+      // Show playing UI.
+    } catch (err) {
+      // Auto-play was prevented
+      // Show paused UI
+      console.error('播放失败', err)
+    }
+    setLoading(false)
+  }
+
+  const isLive = videoUrl.endsWith('.m3u8')
+  useAsync(async () => {
+    if (videoRef.current) {
+      if (isLive) {
+        if (!Hls.isSupported()) {
+          alert('浏览器不支持 HLS')
+        }
+        var hls = new Hls()
+        hls.loadSource(videoUrl)
+        hls.attachMedia(videoRef.current)
+      } else {
+        // 展示第一帧作为封面：https://stackoverflow.com/a/53173104/8242705
+        videoRef.current.src = `${videoUrl}#t=0.5`
+      }
+      await startPlay()
+    }
+  }, [])
+  useAsync(async () => {
+    if (active) {
+      // 从非活跃状态变为活跃状态，开始播放
+      await startPlay()
+    } else {
+      // 从活跃状态变为非活跃状态，开始暂停
+      await videoRef.current?.pause()
+    }
+  }, [active])
 
   return (
     <div className={classnames('bd-player', className)}>
       <div
         className="bd-player-main"
         onClick={async () => {
+          if (!active) return
           if (playing) {
             videoRef.current?.pause()
           } else {
-            await videoRef.current?.play()
+            await startPlay()
           }
         }}
       >
         <video
           autoPlay
-          // TODO: 解决只在视频完全出现之后才进行播放
           className="bd-player-video"
           controls={false}
           id="video"
@@ -58,21 +108,36 @@ export const BDPlayer: React.FC<BDPlayerProps> = ({
           poster={videoPosterUrl}
           preload="metadata"
           ref={videoRef}
+        ></video>
+        <span
+          className="bd-player-icons"
+          // 只有启用的时候才会展示图标
+          style={{ display: active ? 'inline' : 'none' }}
         >
-          {/* TODO: 兼容直播 */}
-          {/* 展示第一帧作为封面：https://stackoverflow.com/a/53173104/8242705 */}
-          <source src={`${videoUrl}#t=0.5`} type="video/mp4"></source>
-        </video>
-        <img
-          className="bd-player-play-icon"
-          src={PlayIcon}
-          style={{
-            // 只有暂停的时候才展示播放按钮
-            visibility: playing ? 'hidden' : 'visible',
-          }}
-        />
+          <img
+            className="bd-player-play-icon"
+            src={PlayIcon}
+            style={{
+              // 只有暂停的时候才展示播放图标
+              opacity: playing ? 0 : 1,
+            }}
+          />
+          <img
+            className="bd-player-loading-icon"
+            src={LoadingIcon}
+            style={{
+              opacity: loading ? 1 : 0,
+            }}
+          />
+        </span>
       </div>
-      <div className="bd-player-controls">
+      <span
+        className="bd-player-controls"
+        style={{
+          // 直播不展示进度条
+          display: isLive ? 'none' : 'inline',
+        }}
+      >
         <div className="bd-player-progress">
           <progress
             className="bd-player-progress-bar"
@@ -96,7 +161,7 @@ export const BDPlayer: React.FC<BDPlayerProps> = ({
             value={Math.floor(currentTime)}
           />
         </div>
-      </div>
+      </span>
     </div>
   )
 }
