482bc1f0760c400c807ab8a5bd3facce99fb7832
Hokori23 <hokori23@qq.com>
2021-02-05 19:01:48 +0800

    fix: 修正路由白名单路径

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -8,7 +8,7 @@ const { cryptoConfig } = config
  */
 const QUERY_METHODS = ['GET', 'DELETE']
 const BODY_METHODS = ['POST', 'PUT']
-const ROUTER_WHITE_LIST = [`user/register`, `user/login`, `test/`].map(
+const ROUTER_WHITE_LIST = [`user/register`, `user/login`, `test`].map(
   (v) => `/api/${v}`,
 )
 const isDev = process.env.NODE_ENV === 'development'
