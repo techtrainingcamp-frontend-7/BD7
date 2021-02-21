b5c2c51e1e401e22230b87a6c47fff003daec580
Hokori23 <hokori23@qq.com>
2021-02-21 15:45:54 +0800

    fix: 修正axios处理异常状态码

--- frontend/src/utils/request/index.ts
+++ frontend/src/utils/request/index.ts
@@ -1,19 +1,22 @@
 import axios, { AxiosRequestConfig } from 'axios'
 import { store } from '@/store'
-import { ACCESS_TOKEN_NAME } from '../const'
+import { ACCESS_TOKEN_NAME, USER_INFO_NAME } from '../const'
 import { Restful } from './type'
 import user from './user'
 import video from './video'
 import upload from './upload'
-
-const whiteList = [/^https:\/\/v0.api.upyun.com\//]
+const whiteList = [/^https:\/\/v0.api.upyun.com/]
+const isWhiteUrl = (url: string) => {
+  return !whiteList.every((reg) => !reg.test(url))
+}
 export const request = async <T>(config: AxiosRequestConfig) => {
   const { dispatch } = store
+  const isWhiteUrlFlag = isWhiteUrl(config.url as string)
   try {
     const token = localStorage.getItem(ACCESS_TOKEN_NAME)
     const headers = config.headers || {}
     // 如果本地有token，每个非白名单请求都附带上token
-    if (token && whiteList.every((reg) => !reg.test(config.url as string))) {
+    if (token && !isWhiteUrlFlag) {
       headers.Authorization = `Bearer ${token}`
     }
     const res = await axios.request<Restful<T>>({
@@ -21,26 +24,30 @@ export const request = async <T>(config: AxiosRequestConfig) => {
       headers,
     })
     if (res.status !== 200) {
-      dispatch.common.SET_DIALOGSTATUS(true)
-      dispatch.common.SET_DIALOGTITLE('警告')
-      dispatch.common.SET_DIALOGCONTENT(
-        `请求失败，状态码：${String(res.status)}`,
-      )
-      if (res.status === 401) {
-        dispatch.login.SET_LOGSTATUS(false)
-      }
-    } else if (res.data.code !== 0) {
-      dispatch.common.SET_DIALOGSTATUS(true)
-      dispatch.common.SET_DIALOGTITLE('警告')
-      // TODO: dev
-      // dispatch.common.SET_DIALOGCONTENT(
-      //   `错误码：${String(res.data.code)}，${res.data.message}`,
-      // )
-      dispatch.common.SET_DIALOGCONTENT(res.data.message)
+      dispatch.common.SET_DIALOG({
+        status: true,
+        title: '警告',
+        content: `请求失败，状态码：${String(res.status)}`,
+      })
+    } else if (
+      (isWhiteUrlFlag && res.data.code !== 200) ||
+      (!isWhiteUrlFlag && res.data.code !== 0)
+    ) {
+      dispatch.common.SET_DIALOG({
+        status: true,
+        title: '警告',
+        content: res.data.message,
+      })
     } else {
-      return res.data.data as T
+      return (res.data.data as T) || ((res.data as unknown) as T)
     }
   } catch (err) {
+    if (err.response.status === 401 && !isWhiteUrlFlag) {
+      localStorage.removeItem(ACCESS_TOKEN_NAME)
+      localStorage.removeItem(USER_INFO_NAME)
+      dispatch.login.SET_LOGSTATUS(false)
+    }
+    console.log(err.response)
     console.error('网络错误', err)
   }
 }
