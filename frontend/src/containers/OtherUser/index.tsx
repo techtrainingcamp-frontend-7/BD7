ed2a7b8f3ac23084d4e05f6ee925cc4367e9cecb
Hokori23 <hokori23@qq.com>
2021-02-22 21:27:54 +0800

    fix: UI优化

--- frontend/src/containers/OtherUser/index.tsx
+++ frontend/src/containers/OtherUser/index.tsx
@@ -25,6 +25,7 @@ const useStyles = makeStyles((theme) => ({
   avatar: {
     width: '100%',
     height: '100%',
+    cursor: 'pointer',
   },
   backdrop: {
     zIndex: theme.zIndex.drawer + 1301,
