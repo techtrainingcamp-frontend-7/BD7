85c0d29312487464ea2e04ed47a68baed4e33d5b
Hokori23 <hokori23@qq.com>
2021-02-24 05:15:16 +0800

    feat: BDPlayer显示报错信息

--- frontend/src/components/BDPlayer/index.tsx
+++ frontend/src/components/BDPlayer/index.tsx
@@ -1,8 +1,8 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, Fragment } from 'react'
 import classNames from 'classnames'
 import PlayIcon from '../static/img/play.svg'
 import LoadingIcon from '../static/img/loading.svg'
-import { useAsync } from 'react-use'
+import { useAsync, useUnmount } from 'react-use'
 import Hls from 'hls.js'
 import { makeStyles } from '@material-ui/core/styles'
 import { Avatar, Typography } from '@material-ui/core'
@@ -60,6 +60,7 @@ export interface BDPlayerProps {
   author?: User
   /* 点击头像回调 */
   onAvatarClick?: Function
+  /* 点赞相关 */
   liked?: boolean
   onLikeChanged?: (liked: boolean) => void
 }
@@ -80,9 +81,18 @@ export const BDPlayer: React.FC<BDPlayerProps> = ({
   const [loading, setLoading] = useState(false)
   const [videoDuration, setVideoDuration] = useState(1)
   const [currentTime, setCurrentTime] = useState(0)
+  const [errorMessage, setErrorMessage] = useState('')
   const videoRef = useRef<HTMLVideoElement>(null)
   // 防止初次加载动画效果
   const [likeChanged, setLikeChanged] = useState(false)
+
+  const handleError = (e: any) => {
+    setErrorMessage(
+      ({
+        hlsError: '直播间不可用或正处于关闭状态',
+      } as any)[String(e)],
+    )
+  }
 
   const startPlay = async () => {
     if (!videoRef.current) return
@@ -96,20 +106,28 @@ export const BDPlayer: React.FC<BDPlayerProps> = ({
       // Auto-play was prevented
       // Show paused UI
       console.log('播放失败', err)
+      if (err.code && err.code !== 20) {
+        handleError(err.message)
+      }
     }
+    // TODO: 在销毁该组件后调用setLoading
+    // 报错: Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function.
     setLoading(false)
   }
 
   const isLive = videoUrl.endsWith('.m3u8')
+  let hls = null as any
   useAsync(async () => {
     if (videoRef.current) {
       if (isLive) {
         if (!Hls.isSupported()) {
           alert('浏览器不支持 HLS')
         }
-        var hls = new Hls()
+        hls = new Hls()
         hls.loadSource(videoUrl)
         hls.attachMedia(videoRef.current)
+
+        hls.on('hlsError', handleError)
       } else {
         // 展示第一帧作为封面：https://stackoverflow.com/a/53173104/8242705
         videoRef.current.src = `${videoUrl}#t=0.5`
@@ -117,6 +135,10 @@ export const BDPlayer: React.FC<BDPlayerProps> = ({
       await startPlay()
     }
   }, [])
+  hls &&
+    useUnmount(() => {
+      hls.off('hlsError')
+    })
   useAsync(async () => {
     if (active) {
       // 从非活跃状态变为活跃状态，开始播放
@@ -129,6 +151,106 @@ export const BDPlayer: React.FC<BDPlayerProps> = ({
 
   return (
     <div className={classNames('bd-player', className)}>
+      {errorMessage ? (
+        <div className="bd-player-error">
+          <Typography variant="subtitle1">{errorMessage}</Typography>
+        </div>
+      ) : (
+        <Fragment>
+          <div className={classes.descriptionBar}>
+            <Typography variant="h6">{description}</Typography>
+          </div>
+          <div
+            className="bd-player-main"
+            onClick={async () => {
+              if (!active) return
+              if (playing) {
+                videoRef.current?.pause()
+              } else {
+                await startPlay()
+              }
+            }}
+          >
+            <video
+              autoPlay
+              className="bd-player-video"
+              controls={false}
+              id="video"
+              loop
+              onLoadedMetadata={() => {
+                setVideoDuration(videoRef.current?.duration || 1)
+                setCurrentTime(videoRef.current?.currentTime || 0)
+              }}
+              onPause={() => {
+                setPlaying(false)
+              }}
+              onPlay={() => {
+                setPlaying(true)
+              }}
+              onTimeUpdate={(evt) => {
+                setCurrentTime(videoRef.current?.currentTime || 0)
+              }}
+              poster={videoPosterUrl}
+              preload="metadata"
+              ref={videoRef}
+            ></video>
+            <span
+              className="bd-player-icons"
+              // 只有启用的时候才会展示图标
+              style={{ display: active ? 'inline' : 'none' }}
+            >
+              <img
+                className="bd-player-play-icon"
+                src={PlayIcon}
+                style={{
+                  // 只有暂停的时候才展示播放图标
+                  opacity: playing ? 0 : 1,
+                }}
+              />
+              <img
+                className="bd-player-loading-icon"
+                src={LoadingIcon}
+                style={{
+                  opacity: loading ? 1 : 0,
+                }}
+              />
+            </span>
+          </div>
+          <span
+            className="bd-player-controls"
+            style={{
+              // 直播不展示进度条
+              display: isLive ? 'none' : 'inline',
+            }}
+          >
+            <div className="bd-player-progress">
+              <progress
+                className="bd-player-progress-bar"
+                max={Math.floor(videoDuration)}
+                value={Math.floor(currentTime)}
+              />
+              <input
+                className="bd-player-progress-seek"
+                max={Math.floor(videoDuration)}
+                onChange={(evt) => {
+                  if (!active) return
+
+                  setCurrentTime(videoRef.current?.currentTime || 0)
+                  if (videoRef.current) {
+                    videoRef.current.currentTime = parseInt(
+                      (evt.target as any).value,
+                    )
+                  }
+                  evt.preventDefault()
+                }}
+                step={1}
+                type="range"
+                value={Math.floor(currentTime)}
+              />
+            </div>
+          </span>
+        </Fragment>
+      )}
       {author && (
         <div className={classes.operationButton}>
           <div className="bd-player-avatar">
@@ -142,7 +264,7 @@ export const BDPlayer: React.FC<BDPlayerProps> = ({
             />
           </div>
 
-          {onLikeChanged ? (
+          {onLikeChanged && (
             <div
               className="bd-player-like"
               onClick={() => {
@@ -157,101 +279,9 @@ export const BDPlayer: React.FC<BDPlayerProps> = ({
                 color={liked ? 'secondary' : 'disabled'}
               />
             </div>
-          ) : null}
+          )}
         </div>
       )}
-      <div className={classes.descriptionBar}>
-        <Typography variant="h6">{description}</Typography>
-      </div>
-      <div
-        className="bd-player-main"
-        onClick={async () => {
-          if (!active) return
-          if (playing) {
-            videoRef.current?.pause()
-          } else {
-            await startPlay()
-          }
-        }}
-      >
-        <video
-          autoPlay
-          className="bd-player-video"
-          controls={false}
-          id="video"
-          loop
-          onLoadedMetadata={() => {
-            setVideoDuration(videoRef.current?.duration || 1)
-            setCurrentTime(videoRef.current?.currentTime || 0)
-          }}
-          onPause={() => {
-            setPlaying(false)
-          }}
-          onPlay={() => {
-            setPlaying(true)
-          }}
-          onTimeUpdate={(evt) => {
-            setCurrentTime(videoRef.current?.currentTime || 0)
-          }}
-          poster={videoPosterUrl}
-          preload="metadata"
-          ref={videoRef}
-        ></video>
-        <span
-          className="bd-player-icons"
-          // 只有启用的时候才会展示图标
-          style={{ display: active ? 'inline' : 'none' }}
-        >
-          <img
-            className="bd-player-play-icon"
-            src={PlayIcon}
-            style={{
-              // 只有暂停的时候才展示播放图标
-              opacity: playing ? 0 : 1,
-            }}
-          />
-          <img
-            className="bd-player-loading-icon"
-            src={LoadingIcon}
-            style={{
-              opacity: loading ? 1 : 0,
-            }}
-          />
-        </span>
-      </div>
-      <span
-        className="bd-player-controls"
-        style={{
-          // 直播不展示进度条
-          display: isLive ? 'none' : 'inline',
-        }}
-      >
-        <div className="bd-player-progress">
-          <progress
-            className="bd-player-progress-bar"
-            max={Math.floor(videoDuration)}
-            value={Math.floor(currentTime)}
-          />
-          <input
-            className="bd-player-progress-seek"
-            max={Math.floor(videoDuration)}
-            onChange={(evt) => {
-              if (!active) return
-
-              setCurrentTime(videoRef.current?.currentTime || 0)
-              if (videoRef.current) {
-                videoRef.current.currentTime = parseInt(
-                  (evt.target as any).value,
-                )
-              }
-              evt.preventDefault()
-            }}
-            step={1}
-            type="range"
-            value={Math.floor(currentTime)}
-          />
-        </div>
-      </span>
     </div>
   )
 }
